2684b98f3f33ebe154a8747fb95753e7cd78b02f
RhysPichanick <37095600+RhysPichanick@users.noreply.github.com>
2018-04-06 16:23:53 +1000

    Add files via upload

---
+++ Final/index.js
@@ -0,0 +1,27 @@
+function SuburbFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+function RatingFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+function filterFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myDropdown");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+        if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+        } else {
+            a[i].style.display = "none";
+        }
+    }
+}
+function myMap() {
+  var mapCanvas = document.getElementById("map");
+  var mapOptions = {
+    center: new google.maps.LatLng(51.5, -0.2), zoom: 10
+  };
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+}
